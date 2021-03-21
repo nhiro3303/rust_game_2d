61866c4ab27d48097a5bf43ca8a812a2037ab45d
nhiro3303 <46623286+nhiro3303@users.noreply.github.com>
2021-03-21 16:01:00 +0900

    add sdl2

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+use std::time::Duration;
+use sdl2::keyboard::Keycode;
+use sdl2::event::Event;
+use sdl2::pixels::Color; 
+
 fn main() {
-    println!("Hello, world!");
+    let sdl_context = sdl2::init().unwrap();
+    let video_subsystem = sdl_context.video().unwrap();
+
+    let window = video_subsystem
+        .window("SDL", 600, 600)
+        .position_centered()
+        .build()
+        .unwrap();
+
+    let mut canvas = window.into_canvas().build().unwrap();
+    canvas.set_draw_color(Color::RGB(255, 255, 255));
+    canvas.clear();
+    canvas.present();
+
+    let mut event_pump = sdl_context.event_pump().unwrap();
+    'running: loop {
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit { .. }
+                | Event::KeyDown {
+                    keycode: Some(Keycode::Escape),
+                    ..
+                } => break 'running,
+                _ => {}
+            }
+        }
+        canvas.present();
+        ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
+    }
 }
